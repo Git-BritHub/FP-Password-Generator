b907c2321997700f1d99d50075be4cf15e8ab123
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-07 01:21:08 -0600

    Added some puedocode for html and js as well as some prompt functions in js.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
 // Assignment code here
+// This is where I want to make string var. Look into split for js, do if statements
+function generatePassword() {
+  console.log ("Here");
+  let count= prompt("How many characters?");
+  let lowerCase = prompt("Do you want lower case (y/n)?");
+  let upperCase= prompt("Do you want upperCase (y/n)");
+  let specialCharacters = prompt("Do you special characters (y/n)");
+  let numeric = prompt("How many numbers?");
+
+  console.log (count, lowerCase, upperCase, specialCharacters,numeric);
+}
 
 
 // Get references to the #generate element
